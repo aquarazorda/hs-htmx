d1c9fad1bd37a9f6c4759e1a0d3eedaf3475a974
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-10-07 05:31:21 +0400

    Added env parsing

--- app/Main.hs
+++ app/Main.hs
@@ -4,23 +4,42 @@
 
 module Main (main) where
 
+import Configuration.Dotenv (defaultConfig, loadFile)
+import Configuration.Dotenv.Environment (lookupEnv)
 import Network.Wai.Handler.Warp (run)
+import Routes.Home (HomeRouter, homeRouter)
+import Routes.Posts (PostsRouter, postsRouter)
 import Servant
-    ( serve,
-      serveDirectoryFileServer,
-      Proxy(..),
-      type (:<|>)(..),
-      Raw,
-      type (:>),
-      Server,
-      Application )
-import Routes.Posts (postsRouter, PostsRouter)
-import Routes.Home (homeRouter, HomeRouter)
+  ( Application,
+    Proxy (..),
+    Raw,
+    Server,
+    serve,
+    serveDirectoryFileServer,
+    type (:<|>) (..),
+    type (:>),
+  )
 
 type API =
   "public" :> Raw
     :<|> HomeRouter
     :<|> PostsRouter
+
+data Env = Env
+  { wpUrl :: String,
+    wpKey :: String,
+    wpSecret :: String
+  } deriving (Show)
+
+parseEnv :: IO (Maybe Env)
+parseEnv = do
+  loadFile defaultConfig
+  mUrl <- lookupEnv "wpUrl"
+  mKey <- lookupEnv "wpKey"
+  mSecret <- lookupEnv "wpSecret"
+  case (mUrl, mKey, mSecret) of
+    (Just url, Just key, Just secret) -> pure $ Just $ Env url key secret
+    _ -> pure Nothing
 
 api :: Proxy API
 api = Proxy
@@ -36,5 +55,7 @@ server =
 
 main :: IO ()
 main = do
+  env <- parseEnv
+  print env
   putStrLn "Running on http://localhost:8080"
   run 8080 app
