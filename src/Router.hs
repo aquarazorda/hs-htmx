b3c5fcb655e52c0afa5e4875b5182afb03ffa214
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2023-10-07 22:54:59 +0400

    Improved posts table

--- src/Router.hs
+++ src/Router.hs
@@ -4,12 +4,11 @@ module Router (routePage) where
 
 import Components.Navbar (navBar)
 import Data.Functor.Identity (Identity)
-import Lucid (Html, HtmlT, body_, class_, div_, head_, href_, id_, link_, main_, rel_)
+import Lucid (Html, HtmlT, body_, class_, head_, href_, id_, link_, main_, rel_)
 import Lucid.Htmx (useHtmx)
 
 routePage :: HtmlT Identity () -> Html ()
 routePage content =
   head_ (useHtmx <> link_ [href_ "/public/styles.css", rel_ "stylesheet"])
     <> body_ [class_ "flex"] navBar
-    <> div_
-      (main_ [class_ "flex-grow p-6", id_ "router-outlet"] content)
+    <> main_ [class_ "flex-grow p-6", id_ "router-outlet"] content
--- src/Routes/Posts.hs
+++ src/Routes/Posts.hs
@@ -6,14 +6,15 @@ module Routes.Posts (postsRouter, PostsRouter) where
 
 import Components.Content.Header (contentHeader)
 import Data.Aeson (decode)
+import qualified Data.WC.Category as Cat
+import Data.WC.Product (WpPost (..), categories, name)
 import Http (getWpResponse)
-import Lucid (Html, class_, div_, ToHtml (toHtml))
+import Lucid (Html, ToHtml (toHtml), button_, class_, div_, span_, table_, tbody_, td_, th_, thead_, tr_)
 import Router (routePage)
 import Servant (Get, Headers, Post, addHeader, (:<|>) (..), (:>))
 import Servant.HTML.Lucid (HTML)
 import Servant.Htmx (HXPush)
 import State (AppM)
-import Data.WC.Product (WpPost(..))
 
 type PostsRouter =
   "posts" :> Get '[HTML] (Html ())
@@ -26,17 +27,41 @@ wcGetPosts = do
 
 content :: AppM (Html ())
 content = do
-    maybe contentEmpty withPosts <$> wcGetPosts
-    where
-        wrapper :: Html() -> Html()
-        wrapper h = contentHeader "Posts" "" <> div_ [class_ "w-full overflow-auto"] h
-        withPosts :: [WpPost] -> Html ()
-        withPosts d = wrapper $ foldl1 (<>) $ fmap postItem d
-        contentEmpty :: Html ()
-        contentEmpty = wrapper "No posts found."
+  maybe contentEmpty withPosts <$> wcGetPosts
+  where
+    wrapper :: Html () -> Html ()
+    wrapper h = contentHeader "Posts" "" <> div_ [class_ "w-full overflow-auto"] h
+    contentEmpty :: Html ()
+    contentEmpty = wrapper "No posts found."
+    withPosts :: [WpPost] -> Html ()
+    withPosts d =
+      wrapper $
+        table_ [class_ "w-full caption-bottom text-sm"] $
+          thead_
+            [class_ "[&amp;_tr]:border-b"]
+            ( tr_ [class_ "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted"] $
+                th_ [class_ "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0"] "Title"
+                  <> th_ [class_ "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0"] "Category"
+                  <> th_ [class_ "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0"] "Quantity"
+                  <> th_ [class_ "h-12 px-4 align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0 text-right"] "Amount"
+                  <> th_ [class_ "h-12 px-4 text-left align-middle font-medium text-muted-foreground [&amp;:has([role=checkbox])]:pr-0"] ""
+            )
+            <> tbody_ [class_ "[&amp;_tr:last-child]:border-0"] postList
+      where
+        postList = foldl1 (<>) $ fmap postItem d
 
 postItem :: WpPost -> Html ()
-postItem (WpPost { name = cName }) = div_ $ toHtml cName
+postItem (WpPost {name = cName, categories = cCategories, price = cPrice}) =
+  tr_ [class_ "border-b transition-colors hover:bg-muted/50 data-[state=selected]:bg-muted"] $
+    td_ [class_ "p-4 align-middle [&amp;:has([role=checkbox])]:pr-0"] (toHtml cName)
+      <> td_ [class_ "p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 flex gap-1"] (foldl1 (<>) $ fmap categoryItem cCategories)
+      <> td_ [class_ "p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 text-right"] (toHtml cPrice)
+      <> td_
+        [class_ "p-4 align-middle [&amp;:has([role=checkbox])]:pr-0 text-right"]
+        (button_ [class_ "inline-flex items-center justify-center text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 px-2 py-1 bg-transparent text-black hover:bg-gray-200 active:bg-gray-300 rounded"] "Edit")
+  where
+    categoryItem :: Cat.WpCategory -> Html ()
+    categoryItem (Cat.WpCategory {Cat.name = cat}) = span_ [class_ "px-2 py-1 bg-red-200 text-red-800 rounded-md"] $ toHtml cat
 
 type GET = AppM (Html ())
 
